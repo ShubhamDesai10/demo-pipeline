e849e80214dfbd42f9469380dba5a5fe1b055c86
Shubham Desai <59305393+ShubhamDesai10@users.noreply.github.com>
2021-10-06 13:42:48 +0530

    Update getapi.py

--- getapi.py
+++ getapi.py
@@ -1,13 +1,18 @@
 import boto3
 
-
-clientObj = boto3.client('apigateway')
-
-
-jsonObj = clientObj.get_rest_apis(limit=10)
+def getApiURL():
+    clientObj = boto3.client('apigateway')
 
 
-for x in jsonObj['items']:
-    if x['name'] == "lambda-api":
-        print(x['id'])
+    jsonObj = clientObj.get_rest_apis(limit=10)
+
+    for x in jsonObj['items']:
+        if x['name'] == "lambda-api":
+            global id
+            id = x['id']
+
+    api_url = "https://{}.execute-api.us-east-2.amazonaws.com/v0/lambda".format(id)
+    return api_url
+
+
 
