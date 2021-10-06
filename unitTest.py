83de883101b28f2adb3fb24cd8b7f5c23e72a633
Shubham Desai <59305393+ShubhamDesai10@users.noreply.github.com>
2021-10-06 15:17:36 +0530

    Update unitTest.py

--- unitTest.py
+++ unitTest.py
@@ -5,7 +5,7 @@ class ConfigTestCase(unittest.TestCase):
     def testResponse(self):
         try:
             response = responseValidation()
-            self.assertTrue(response == 200)
+            self.assertEqual(response, 200)
         except Exception as e:
             print(e)
 
