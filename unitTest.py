f139f4a2c093859541f193f74951670d0f9073e0
Shubham Desai <59305393+ShubhamDesai10@users.noreply.github.com>
2021-10-06 15:55:39 +0530

    Update unitTest.py

--- unitTest.py
+++ unitTest.py
@@ -4,13 +4,9 @@ import getapi
 
 class ConfigTestCase(unittest.TestCase):
     def testResponse(self):
-        try:
-            response = responseValidation()
-            self.assertEqual(response, 200)
-        except Exception as e:
-            print("Test didn't pass!")
-            getapi.deleteApi()
-            print("Stack Deleted")
+        response = responseValidation()
+        self.assertEqual(response, 200)
+       
 
 
 if __name__ == "__main__":
