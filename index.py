5686798c1d2415ad9d34d0ab22abbd8605818453
Shubham Desai <59305393+ShubhamDesai10@users.noreply.github.com>
2021-10-07 17:18:29 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,6 +4,6 @@ def handler(event, context):
               'statusCode': 200,
               'headers': {},
               'multiValueHeaders': {},
-              'body': 'Hello, World!!!!'
+              'body': 'Hello, World! Hello India'
             }
             return response
